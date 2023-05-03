622da5029f5e9e6ef1543e86742f82ebd16e6413
Garth Humphreys <g.humphreys1@mac.com>
2023-05-02 20:03:03 -0500

    refactored code to better parse the json

--- certy.go
+++ certy.go
@@ -1,11 +1,17 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
-	"github.com/gocolly/colly"
+	"io/ioutil"
+	"net/http"
 	"os"
 )
+
+type CRTShCertificate struct {
+	NameValue string `json:"common_name"`
+}
 
 func main() {
 	domainPtr := flag.String("domain", "garthhumphreys.com", "a domain")
@@ -26,22 +32,45 @@ func main() {
 	}
 }
 
-func certSearch(domain string)  {
-	c := colly.NewCollector(
-		colly.UserAgent("Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0"),
-		)
-	c.OnHTML("table:nth-child(8) > tbody > tr > td > table > tbody > tr > td:nth-child(5)", func(element *colly.HTMLElement) {
-		// TODO:
-		// - add domains to an array and remove duplicates
-		// - print to screen only the unique domains
-		fmt.Println(element.Text)
-		// fmt.Printf("title:%s \n link:%s \r", element.Text, element.Attr("href"))
-	})
+func certSearch(domain string) {
+	// domain := "example.com"
+	url := fmt.Sprintf("https://crt.sh/?q=%s.%s&output=json", "%", domain)
+	fmt.Println("URL:", url)
 
-	c.OnRequest(func(request *colly.Request) {
-		fmt.Println("Visting", request.URL)
-	})
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+	defer resp.Body.Close()
 
-	query := "https://crt.sh/?q=" + domain
-	c.Visit(query)
+	fmt.Println("RESP:", resp)
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+	var certificates []CRTShCertificate
+	err = json.Unmarshal(body, &certificates)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+	unique := make(map[string]bool)
+	for _, certificate := range certificates {
+		if _, ok := unique[certificate.NameValue]; !ok {
+			unique[certificate.NameValue] = true
+			fmt.Println(certificate.NameValue)
+		}
+	}
+
+	// prettyJSON, err := json.MarshalIndent(data, "", "  ")
+	// if err != nil {
+	// 	fmt.Println("Error:", err)
+	// 	return
+	// }
+	//
+	// fmt.Println(string(prettyJSON))
 }
